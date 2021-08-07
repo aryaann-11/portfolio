4e257f56239b732970e8fee41113ee094155fb8c
aryaann-11 <aryansprivatemail@gmail.com>
2021-08-07 16:40:18 +0530

    initial commit

---
+++ config/sanityConfig.js
@@ -0,0 +1,6 @@
+import sanityClient from "@sanity/client"
+
+export default sanityClient({
+    projectId: "mrf8mkz7",
+    dataset: "production"
+})
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import React from "react"
+export default function Home() {
+  return (
+    <>
+    </>
+  )
+}
