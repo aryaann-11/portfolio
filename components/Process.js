7a43affa9414b1ce4d31e23c62e0ed3c3fb393ac
aryaann-11 <aryansprivatemail@gmail.com>
2021-08-13 18:05:41 +0530

    done with process

--- components/Process.js
+++ components/Process.js
@@ -10,12 +10,12 @@ export default function Process({ stages }) {
                         <h2 className="mt-5">The Process</h2>
                     </div>
                 </div>
-                <div className="row mb-5 mt-5">
+                <div className="row mb-5 mt-5 justify-content-around">
                     {
                         stages.map((stage) => {
                             return (
                                 <>
-                                    <div className="col text-center">
+                                    <div className="col-md-2 col-sm-12 text-center">
                                         <div className={`mx-auto mb-3 ${styles.numberCircle}`}>{stage.stageNumber}</div>
                                         <h5 className="fw-bold">{stage.title}</h5>
                                         <img src={stage.image_url} alt={stage.title} className={styles.stage_image} />
